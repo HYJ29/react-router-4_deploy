42409e02a3910927450d59b53ad4d5b86d892508
HYJ <teen14y@naver.com>
2019-07-09 18:44:47 +0900

    basic rounting with react-router-dom

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
+import Header from './Header';
+import Featured from './Featured';
+import NotFound from './NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' render={() => <About title='titlte'/>} />
+        <Route exact path='/teachers' component={Teachers} />
+        <Route path='/teachers/:topic/:name' component={Featured} />
+        <Route path='/courses' component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+
+);
+
+export default App;
